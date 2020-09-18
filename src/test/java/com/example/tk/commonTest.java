b506313bbccb6d81682b7f271536604a8a47cf24
tutiankun <tutiankun@accesscorporate.com.cn>
2020-09-18 20:09:51 +0800

    测试

---
+++ src/test/java/com/example/tk/commonTest.java
@@ -0,0 +1,13 @@
+package com.example.tk;
+
+import org.junit.Test;
+
+public class commonTest extends BaseTest {
+
+    @Test
+    public void test1(){
+        String key="F145G42&555&5881664as";
+
+    }
+
+}
