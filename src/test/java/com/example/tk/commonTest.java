6f38d984bd526bc5706937e9178ebd96823b754b
tutiankun <tutiankun@accesscorporate.com.cn>
2020-09-22 16:57:47 +0800

    测试

--- src/test/java/com/example/tk/commonTest.java
+++ src/test/java/com/example/tk/commonTest.java
@@ -2,11 +2,12 @@ package com.example.tk;
 
 import org.junit.Test;
 
-public class commonTest extends BaseTest {
+public class commonTest {
 
     @Test
     public void test1(){
-        String key="F145G42&555&5881664as";
+        String key=String.format("%s&%s&%s","天坤","你好","吗");
+        System.out.println(key);
 
     }
 
