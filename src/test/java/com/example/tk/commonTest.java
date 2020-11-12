d80c5ede1c48307382dbbdf563229df74b3f161e
tutiankun <tutiankun@accesscorporate.com.cn>
2020-11-12 11:04:12 +0800

    状态码映射

--- src/test/java/com/example/tk/commonTest.java
+++ src/test/java/com/example/tk/commonTest.java
@@ -5,8 +5,11 @@ import com.example.tk.vo.Person;
 import com.google.common.collect.Lists;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class commonTest {
 
@@ -29,7 +32,27 @@ public class commonTest {
 
     @Test
     public void test3(){
+        int a=3;
+        for (int i = 0; i < 5; i++) {
+            System.out.println(i);
+            if (i==a){
+                return;
+            }
+            System.out.println("i="+i);
+        }
+        System.out.println("测试");
+
     }
+
+    @Test
+    public void test(){
+        ArrayList<Person> list = new ArrayList<>();
+        list=null;
+        Map<Integer, String> map = list.stream().collect(Collectors.toMap(Person::getAge, Person::getName));
+        System.out.println(map);
+
+    }
+
 
 
 }
